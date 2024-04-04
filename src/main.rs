2dabe096272938833a88b59b52cb1996e3e1af71
George <george@fozzie.dev>
2024-04-04 12:29:54 +0100

    Format

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use std::env;
 use rpassword::prompt_password;
+use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -10,12 +10,12 @@ fn main() {
         std::process::exit(1);
     }
 
-    let input_string = prompt_password("String: ")
-        .expect("Failed to read input.");
+    let input_string = prompt_password("String: ").expect("Failed to read input.");
 
     let chars: Vec<char> = input_string.chars().collect();
 
-    let positions = args[1].split(',')
+    let positions = args[1]
+        .split(',')
         .filter_map(|p| p.parse::<usize>().ok())
         .collect::<Vec<_>>();
 
